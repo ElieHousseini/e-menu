88937befbbd5830fa8a89e8b118e7d0f68129c71
ElieHousseini <eliashousseini@gmail.com>
2023-05-24 11:28:01 +0400

    move SVGs to seperate folder

---
+++ src/assets/Icons/Seach.tsx
@@ -0,0 +1,15 @@
+const SearchIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 2a8 8 0 0 1 6.32 12.906l5.387 5.387-1.414 1.414-5.387-5.387A8 8 0 1 1 10 2zm0 2a6 6 0 1 0 0 12A6 6 0 0 0 10 4z" />
+    </svg>
+  );
+};
+
+export default SearchIcon;
--- src/components/Menu/Menu/Menu.tsx
+++ src/components/Menu/Menu/Menu.tsx
@@ -1,11 +1,14 @@
+import styles from "./Menu.module.css";
 import { useEffect, useState, ChangeEvent, FormEvent } from "react";
+import { useTranslation } from "react-i18next";
 import { MenuCategoryType } from "../../../types";
 import fetchData from "../../../services/fetch";
-import styles from "./Menu.module.css";
 import MenuCategory from "../MenuCategory/MenuCategory";
-
 import Dropdown from "../Dropdown/Dropdown";
-import { useTranslation } from "react-i18next";
+import LocationIcon from "../../../assets/Icons/Location.tsx";
+import PhoneIcon from "../../../assets/Icons/Phone.tsx";
+import SearchIcon from "../../../assets/Icons/Seach.tsx";
+import WifiIcon from "../../../assets/Icons/Wifi.tsx";
 
 type LanguageOption = {
   value: string;
@@ -17,8 +20,8 @@ const Menu = () => {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
   const FETCH_CATEGORIES_BAR_URL = import.meta.env
     .VITE_FETCH_CATEGORIES_BAR_URL;
-  const { i18n, t } = useTranslation();
 
+  const { i18n, t } = useTranslation();
   const [categoriesData, setCategoriesData] = useState<MenuCategoryType[]>([]);
   const [toggleBtn, setToggleBtn] = useState(false);
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -27,8 +30,11 @@ const Menu = () => {
     { value: "en", label: "English" },
     { value: "ru", label: "Russian" },
   ];
-
   const defaultLanguageOption = languageOptions[0];
+
+  const LOCATION_URL = "https://www.google.com/maps/place/Beirut";
+  const HEADER_IMAGE_URL = `${BASE_URL}/uploads/restaurant_cf71f55443.jpg)`;
+  const WIFI_PASSWORD = "WiFi_Password123";
 
   useEffect(() => {
     fetchData({
@@ -52,126 +58,13 @@ const Menu = () => {
     i18n.changeLanguage(selectedOption.value);
   };
 
-  const SearchIconImage = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="24"
-        viewBox="0 0 24 24"
-        width="24"
-      >
-        <path d="M0 0h24v24H0z" fill="none" />
-        <path d="M10 2a8 8 0 0 1 6.32 12.906l5.387 5.387-1.414 1.414-5.387-5.387A8 8 0 1 1 10 2zm0 2a6 6 0 1 0 0 12A6 6 0 0 0 10 4z" />
-      </svg>
-    );
-  };
-
-  const LocationIconImage = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 256"
-        width="16"
-        height="16"
-        fill="currentColor"
-      >
-        {" "}
-        <g>
-          <circle
-            cx="128"
-            cy="104"
-            r="32"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          ></circle>{" "}
-          <path
-            d="M208,104c0,72-80,128-80,128S48,176,48,104a80,80,0,0,1,160,0Z"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          ></path>
-        </g>
-      </svg>
-    );
-  };
-
-  const PhoneImageSVG = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 256"
-        width="16"
-        height="16"
-        fill="currentColor"
-      >
-        {" "}
-        <g>
-          <path
-            d="M92.47629,124.81528a84.34782,84.34782,0,0,0,39.05334,38.8759,7.92754,7.92754,0,0,0,7.8287-.59231L164.394,146.40453a8,8,0,0,1,7.58966-.69723l46.837,20.073A7.97345,7.97345,0,0,1,223.619,174.077,48.00882,48.00882,0,0,1,176,216,136,136,0,0,1,40,80,48.00882,48.00882,0,0,1,81.923,32.381a7.97345,7.97345,0,0,1,8.29668,4.79823L110.31019,84.0571a8,8,0,0,1-.65931,7.53226L93.01449,117.00909A7.9287,7.9287,0,0,0,92.47629,124.81528Z"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          ></path>
-        </g>
-      </svg>
-    );
-  };
-
-  const WifiIconImage = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 256"
-        width="16"
-        height="16"
-        fill="currentColor"
-      >
-        {" "}
-        <g>
-          <path
-            d="M92.91969,166.06177a50.7769,50.7769,0,0,1,70.145,0"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          ></path>{" "}
-          <path
-            d="M58.97857,132.12064a98.75415,98.75415,0,0,1,138.02724,0"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          ></path>{" "}
-          <path
-            d="M25.06379,98.17952a146.68225,146.68225,0,0,1,205.8568,0"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="16"
-          ></path>{" "}
-          <circle cx="128" cy="200" r="12"></circle>
-        </g>
-      </svg>
-    );
-  };
-
   return (
     <div className={styles.menu}>
       <div className={styles.header}>
         <div
           className={styles.headerImage}
           style={{
-            backgroundImage: `url(${BASE_URL}/uploads/restaurant_cf71f55443.jpg)`,
+            backgroundImage: `url(${HEADER_IMAGE_URL})`,
           }}
         />
         <Dropdown
@@ -185,27 +78,27 @@ const Menu = () => {
           <h1 className={styles.title}>{t("restaurant_name")}</h1>
           <div className={styles.main_contact}>
             <a
-              href="https://www.google.com/maps/place/Beirut"
+              href={`${LOCATION_URL}`}
               target="_blank"
               title="location on google maps"
               rel="noreferrer"
               className={`${styles.location} ${styles.elementsContainer}`}
             >
-              <LocationIconImage />
+              <LocationIcon />
               {t("location")}
             </a>
             <a
               href={`tel:${t("phone")}`}
               className={`${styles.phone} ${styles.elementsContainer}`}
             >
-              <PhoneImageSVG />
+              <PhoneIcon />
               {t("phone")}
             </a>
             <span
               className={`${styles.wifiPassword} ${styles.elementsContainer}`}
             >
-              <WifiIconImage />
-              WiFi_Password123
+              <WifiIcon />
+              {WIFI_PASSWORD}
             </span>
           </div>
           <span>
@@ -226,25 +119,23 @@ const Menu = () => {
               <b>{t("bar")}</b>
             </button>
           </span>
-          <div>
-            <form onSubmit={handleSubmit} className={styles.searchBar}>
-              <div className={styles.searchInputGroup}>
-                <input
-                  type="text"
-                  placeholder={t("search_for_dishes") as string}
-                  value={searchTerm}
-                  onChange={handleChange}
-                  className={styles.searchInput}
-                />
-                <div className={styles.searchButton}>
-                  <SearchIconImage />
-                </div>
+          <form onSubmit={handleSubmit} className={styles.searchBar}>
+            <div className={styles.searchInputGroup}>
+              <input
+                type="text"
+                placeholder={t("search_for_dishes") as string}
+                value={searchTerm}
+                onChange={handleChange}
+                className={styles.searchInput}
+              />
+              <div className={styles.searchButton}>
+                <SearchIcon />
               </div>
-            </form>
-            {categoriesData?.map((category) => (
-              <MenuCategory key={category.id} category={category} />
-            ))}
-          </div>
+            </div>
+          </form>
+          {categoriesData?.map((category) => (
+            <MenuCategory key={category.id} category={category} />
+          ))}
         </div>
         <div className={styles.footer}>
           <p>{t("thank_you_for_visiting_us")}</p>
